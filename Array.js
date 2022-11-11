e6374be0778d8a6db5dec336a603c486e786441a
Sk_Mustafa_M8 <61942973+skmustafa@users.noreply.github.com>
2022-11-11 15:19:23 +0530

    Update Array.js

--- Array.js
+++ Array.js
@@ -1,7 +1,48 @@
-const dev = ['abc','def','ghi']
+
+// Basic One
+// const wizard = ['Harry','Potter']
+// const [firstName,lastName] = wizard
+// console.log(firstName,lastName)
+
+// Skipping Values
+// const wizard = ['Harry', 'James', 'Potter']
+// const [firstName, ,lastName] = wizard
+// console.log(firstName,lastName)
+
+// Index Position Values
+// const developer = ['Rajneesh', 'Mithilesh','Chaubey']
+// const { [1]: middlename } = developer
+// console.log(middlename)
+
+
+// Default Values
+
+// const address =  ['Pittsburgh','Pennsylvania']
+// const [city,state,country = 'US'] = address
+// console.log(city,country,state)
+// Here, we are making the default value of the country variable to US, since our address array don't have any country by default the country will be US.
+
+// Swapping Values
+//  let a = 10, b = 20;
+// [ a, b] = [b, a]
+// console.log(a,b)  
+
+
+// Returning Value From Function
+
+// const getLocation = () => [18.43212,72.87211]
+// const [lat,lng] = getLocation()
+// console.log(lat,lng)
+
+// // with spread operator
+// const languages = ['javascript','c++','python','c#','java','c']
+// const[lang1,lang2,...others] = languages
+// console.log(lang1,lang2,others)
+
+// const dev = ['abc','def','ghi']
  
-const { [0]:middleName} = dev
-console.log(middleName)
+// const { [0]:middleName} = dev
+// console.log(middleName)
 
 // const address = ['Mumbai','Maha'] 
 
